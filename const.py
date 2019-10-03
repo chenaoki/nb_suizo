0682dea443e5cf34baa25058fbaa3120346e8503
chenaoki <cheanoki@gmail.com>
2019-10-03 12:13:58 +0000

    initial commit

---
+++ const.py
@@ -0,0 +1,7 @@
+src_path = "/mnt/Omer/Project/14.SuizoAssyuku/data/"
+dst_path = "/mnt/Omer/Project/14.SuizoAssyuku/result/"
+left = 50
+top = 100
+width = 800
+height = 800
+fps = 30
---
+++ params.py
@@ -0,0 +1,122 @@
+params = {
+    "20170905-1" :{
+        "before":{
+            "path" : "3_CMG0_HIGAIN3_add_10~20min.avi",
+            "start" : 15,
+            "nframe" : 300,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_20~30min.avi",
+            "start" :21,
+            "nframe" : 180,
+        }
+    },
+    "20170921-1" :{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~10min.avi",
+            "start" : 13,
+            "nframe" : 300,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_20~30min.avi",
+            "start" : 13,
+            "nframe" : 300,
+        }
+    },
+    "20170921-2" :{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~10min.avi",
+            "start" : 13,
+            "nframe" : 300,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_20~30min.avi",
+            "start" : 31,
+            "nframe" : 300,
+        }
+    },
+    "20170925-1" :{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~10min.avi",
+            "nframe" : 300,
+            "start" : 16,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_20~30min.avi",
+            "nframe" : 300,
+            "start" : 27,
+        }
+    },
+    "20171020-1" :{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~5min.avi",
+            "nframe" : 240,
+            "start" : 30,
+        },
+        "after":{
+            "path" : "4_CMG0_HIGAIN3_compress_add_5~10min.avi",
+            "start" : 30,
+            "nframe" : 240,
+        }
+    },
+    "20171020-2":{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~5min.avi",
+            "start" : 16,
+            "nframe" : 240,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_10~15min.avi",
+            "start" : 0,
+            "nframe" : 240,
+        }
+    },
+    "20171020-3":{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~5min.avi",
+            "start" : 19,
+            "nframe" : 240,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_10~15min.avi",
+            "start" : 15,
+            "nframe" : 240,
+        }
+    },
+    "20171117-1":{
+        "before":{
+            "path" : "3_CMG0_HIGAIN3_add_5~10min.avi",
+            "start" : 8,
+            "nframe" : 240,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_10~15min.avi",
+            "start" : 0,
+            "nframe" : 300
+        }
+    },
+    "20171120-2":{
+        "before":{
+            "path" : "3_CMG0_HIGAIN3_add_5~10min.avi",
+            "start" : 30,
+            "nframe" : 240,
+        },
+        "after":{
+            "path" : "5_CMG0_HIGAIN3_add_compress_add_10~15min.avi",
+            "start" : 30,
+            "nframe" : 240,
+        }
+    },
+    "20171120-3":{
+        "before":{
+            "path" : "2_CMG0_HIGAIN3_0~5min.avi",
+            "start" : 10,
+            "nframe" : 240
+        },
+        "after":{
+            "path" : "6_CMG0_HIGAIN3_add_compress_add_add_15~20min.avi",
+            "start" : 13,
+            "nframe" : 240
+        }
+    },
+}
